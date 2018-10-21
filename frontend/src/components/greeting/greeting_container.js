747ccedfbedc63de5c73e9270fc2144b9b2503f2
Peter <peter.niemeier@gmail.com>
2018-10-21 14:01:50 -0700

    Add frontend

---
+++ frontend/src/components/greeting/greeting_container.js
@@ -0,0 +1,21 @@
+// frontend/src/components/greeting/greeting_container.js
+
+import { connect } from 'react-redux';
+
+import { logoutUser } from '../../util/session_api_util';
+import Greeting from './greeting';
+
+const mapStateToProps = ({ session }) => {
+  return {
+    currentUser: session
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logoutUser())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Greeting);

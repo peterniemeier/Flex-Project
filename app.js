e6506f25fdabcc3a9cadf37abc3e3b18287940df
anthony <anthonyltam@gmail.com>
2018-10-22 12:07:06 -0700

    Login and signup works for user authe backend

--- app.js
+++ app.js
@@ -3,28 +3,29 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const db = require('./config/keys').mongoURI;
-mongoose
-  .connect(db)
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
-const port = process.env.PORT || 5000;
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const passport = require('passport');
 require('./config/passport');
+const port = process.env.PORT || 5000;
 
-// app.use((req, res, next) => {
-// next();
-// });
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
 app.use(passport.initialize());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req,res) => {
-  res.send('Shazam!');
+  // res.send('feebo');
+  res.json({hi: 'feebo'});
 });
 
-app.use("/api/users", users)
+app.use("/api/users", users);
 
 
-app.listen(port, () => console.log(`Server is running on port:${port}`));
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));

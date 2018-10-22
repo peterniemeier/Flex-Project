46230106a662dc8206d87983ada52e25768cc3da
Peter <peter.niemeier@gmail.com>
2018-10-21 17:38:44 -0700

    Prep for final push to master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import greeting from './greeting/greeting_container';
 
 class App extends Component {
   render() {

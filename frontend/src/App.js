915e90239e6a5ad32032602635ea582c452a4a78
Peter <peter.niemeier@gmail.com>
2018-10-23 11:14:42 -0700

    Update util

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
 import Greeting from './components/greeting/greeting_container';
 import MainPage from './components/main_page/main_page';
+import ToiletFormContainer from './components/toilet_form/toilet_form_container';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
       <Greeting />
       <Route exact path='/' component={MainPage}/>
+      <Route exact path='toilets/create' component={ToiletFormContainer} />
       </div>
     );
   }

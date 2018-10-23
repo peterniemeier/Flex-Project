7936ae3a2f9d8408db88c55b1812c858586eede6
anthony <anthonyltam@gmail.com>
2018-10-23 07:14:39 -0700

    Remove conflict in app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import Greeting from './components/greeting/greeting_container';
 // import Test from './components/test';
-=======
-import logo from './logo.svg';
-import './App.css';
-import greeting from './greeting/greeting_container';
->>>>>>> 46230106a662dc8206d87983ada52e25768cc3da
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Greeting />
-        
+        <Greeting />
       </div>
     );
   }

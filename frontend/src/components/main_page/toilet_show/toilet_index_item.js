5936db1a4e1147cee31a15d407b54209b04fb1cf
Austin <austin.cotant@gmail.com>
2018-10-25 16:03:12 -0700

    Add styling and links to main page selected toilet

--- frontend/src/components/main_page/toilet_show/toilet_index_item.js
+++ frontend/src/components/main_page/toilet_show/toilet_index_item.js
@@ -8,12 +8,12 @@ export class ToiletIndexItem extends Component {
 
   render() {
     const { toilet } = this.props;
-    return (
-      <li onClick={this.handleClick.bind(this)} className='index-item'>
-        <strong>{toilet.title}</strong>
+    return <li className="index-item">
+        <strong onClick={this.handleClick.bind(this)}>
+          {toilet.title}
+        </strong>
         <p>{toilet.address}</p>
-      </li>
-    )
+      </li>;
   }
 }
 

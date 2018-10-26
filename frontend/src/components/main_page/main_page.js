d5ddfb8101ddba6bbe85a5c40340faee9fa149bf
Austin <austin.cotant@gmail.com>
2018-10-26 12:02:51 -0700

    Make searchbar into a component and change styling to work more modularly

--- frontend/src/components/main_page/main_page.js
+++ frontend/src/components/main_page/main_page.js
@@ -7,6 +7,7 @@ import Map from '../maps/map';
 import ToiletCreator from './toilet_creator';
 import ToiletIndex from './toilet_show/toilet_index';
 import SelectedToilet from './toilet_show/selected_toilet';
+import SearchBar from '../splash/search_bar';
 
 class MainPage extends React.Component {
     constructor(props) {
@@ -16,19 +17,24 @@ class MainPage extends React.Component {
     render() {
         const { history } = this.props;
         return (
-            <div className='all-content'>
-                <div className='big-map'>
-                    <Map history={history}/>
+            <div>
+                <div className='search-container-main'>
+                    <SearchBar />
                 </div>
-                <div className='toilet-sidebar'>
-                    <ToiletCreator history={history}/>
-                    <Switch >
-                        <div className='toilet-index'>
-                            <Route path='/main/:toiletId' history={history}
-                                component={SelectedToilet} toilets={this.props.toilets}/>
-                            <Route path='/main' component={ToiletIndex} history={history}/>
-                        </div>
-                    </Switch>
+                <div className='all-content'>
+                    <div className='big-map'>
+                        <Map history={history}/>
+                    </div>
+                    <div className='toilet-sidebar'>
+                        <ToiletCreator history={history}/>
+                        <Switch >
+                            <div className='toilet-index'>
+                                <Route path='/main/:toiletId' history={history}
+                                    component={SelectedToilet} toilets={this.props.toilets}/>
+                                <Route path='/main' component={ToiletIndex} history={history}/>
+                            </div>
+                        </Switch>
+                    </div>
                 </div>
             </div>
         );
---
+++ frontend/src/components/splash/search_bar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux';
+
+import { setCenter } from "../../actions/map_actions";
+import { openModal } from "../../actions/modal_actions";
+
+import './searchbar.css';
+
+const google = window.google;
+
+export class SearchBar extends Component {
+
+    componentDidMount() {
+        const input = document.getElementById('searchbar');
+        this.autocomplete = new google.maps.places.Autocomplete(input);
+    }
+
+    search(event) {
+        const { setCenter, history } = this.props;
+        event.preventDefault();
+        if (this.autocomplete.getPlace()) {
+            const location = this.autocomplete.getPlace().geometry.location;
+            const latLng = {
+                lat: location.lat(),
+                lng: location.lng(),
+            }
+            setCenter(latLng);
+            history.push('/main');
+        }
+    }
+
+    render() {
+        return (
+            <form className='search-bar'>
+                <input className="searchbar" id='searchbar' placeholder="Find a Place To Rest Your Weary Bottom" />
+                <i onClick={this.search.bind(this)} className="fas fa-search" />
+            </form>
+        )
+    }
+}
+
+const mDtP = dispatch => ({
+    setCenter: center => dispatch(setCenter(center)),
+    openModal: modal => dispatch(openModal(modal))
+});
+
+export default connect(null, mDtP)(SearchBar);

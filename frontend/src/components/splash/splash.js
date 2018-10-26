ab0c6f817f5f8eba5017ddfaec687612186ee56f
Austin <austin.cotant@gmail.com>
2018-10-25 19:01:57 -0700

    Create and style searchbar on the main page

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -4,12 +4,21 @@ import '../../assets/stylesheets/shared.css';
 import './splash.css';
 
 export class Splash extends Component {
+
+  search() {
+
+  }
+
   render() {
-    return (
-        <div className="splash-container">
-            <img src="https://images.pexels.com/photos/127726/pexels-photo-127726.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+    return <div className="splash-container">
+        <form className="search-bar">
+          <input className="searchbar" placeholder="Find a Place To Rest Your Weary Bottom" />
+          <i onClick={this.search.bind(this)} className="fas fa-search" />
+        </form>
+        <div className="splash-image-container">
+          <img src="https://images.pexels.com/photos/127726/pexels-photo-127726.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
         </div>
-    );
+      </div>;
   }
 }
 

1713333bd4d9e26a2442fe464e05f303933ec3a1
Austin <austin.cotant@gmail.com>
2018-10-26 09:03:37 -0700

    Add google searchbar to splashpage

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 import '../../assets/stylesheets/shared.css';
-import './splash.css';
+import './splash.css'; 
+
+import { setCenter } from '../../actions/map_actions';
+const google = window.google;
 
 export class Splash extends Component {
 
-  search() {
+  constructor(props) {
+    super(props) 
+  }
 
+  componentDidMount() {
+    const input = document.getElementById('searchbar');
+    this.autocomplete = new google.maps.places.Autocomplete(input);
+  }
+
+  search(event) {
+    const { setCenter, history } = this.props;
+    event.preventDefault();
+    if (this.autocomplete.getPlace()) {
+      const location = this.autocomplete.getPlace().geometry.location;
+      const latLng = {
+        lat: location.lat(),
+        lng: location.lng(),
+      }
+      // debugger;
+      setCenter(latLng);
+      history.push('/main');
+    }
   }
 
   render() {
     return <div className="splash-container">
         <form className="search-bar">
-          <input className="searchbar" placeholder="Find a Place To Rest Your Weary Bottom" />
+          <input className="searchbar" id='searchbar' placeholder="Find a Place To Rest Your Weary Bottom" />
           <i onClick={this.search.bind(this)} className="fas fa-search" />
         </form>
         <div className="splash-image-container">
@@ -22,4 +46,8 @@ export class Splash extends Component {
   }
 }
 
-export default Splash
+const mDtP = dispatch => ({
+  setCenter: center => dispatch(setCenter(center)),
+})
+
+export default connect(null, mDtP)(Splash);
--- frontend/src/reducers/maps_reducer.js
+++ frontend/src/reducers/maps_reducer.js
@@ -1,9 +1,16 @@
 import {
     RECEIVE_TOILET_POS,
     RECEIVE_TOILET_ADDRESS,
+    SET_CENTER,
 } from '../actions/map_actions';
 
-export default (state = {toiletPos: null, toiletAddress: null}, action) => {
+const defaultState = { 
+    toiletPos: null, 
+    toiletAddress: null,
+    center: { lat: 37.7749, lng: -122.4194 }
+     }
+
+export default (state = defaultState, action) => {
     Object.freeze(state) 
     const newState = Object.assign({}, state);
     
@@ -13,6 +20,9 @@ export default (state = {toiletPos: null, toiletAddress: null}, action) => {
             return newState;
         case RECEIVE_TOILET_ADDRESS: 
             newState.toiletAddress = action.address;
+            return newState;
+        case SET_CENTER: 
+            newState.center = action.center; 
             return newState;    
         default:
             return state;

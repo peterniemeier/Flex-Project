f76e6531752ea03dd7b0ad6e48f1000e0893b2f0
anthony <anthonyltam@gmail.com>
2018-10-28 15:16:25 -0700

    Ready for heroku push

--- frontend/src/util/map_api_util.js
+++ frontend/src/util/map_api_util.js
@@ -2,7 +2,7 @@
 const $ = window.$;
 export const fetchLocation = addressStr => (
     $.ajax({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressStr}&key=${window.api_key}`,
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressStr}&key=${process.env.GOOGLE_API_KEY}`,
         method: 'GET',
     })
 );
@@ -10,13 +10,15 @@ export const fetchLocation = addressStr => (
 export const fetchAddress = latLng => {
     const latLngString = `${latLng.lat},${latLng.lng}`;
     return $.ajax({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latLngString}&key=${window.api_key}`,
-        method: 'GET'
-    })
+      url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latLngString}&key=${
+        process.env.GOOGLE_API_KEY
+      }`,
+      method: "GET"
+    });
 };
 
 // markers=icon:<icon_url>|latlng
 export const makeMapUrl = (toilet) => {
     const toiletLatLng = `${toilet.lat},${toilet.lng}`;
-    return `https://maps.googleapis.com/maps/api/staticmap?center=${toiletLatLng}&zoom=17&size=300x300&markers=color:red%7C${toiletLatLng}&key=${window.api_key}`
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${toiletLatLng}&zoom=17&size=300x300&markers=color:red%7C${toiletLatLng}&key=${process.env.GOOGLE_API_KEY}`;
 }

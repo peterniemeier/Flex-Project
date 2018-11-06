1a248ed920b32779f9de18d43d2de8ab316dc67c
Austin <austin.cotant@gmail.com>
2018-11-06 13:55:07 -0800

    Create footer files

---
+++ frontend/src/components/splash/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './footer.css';
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Footer

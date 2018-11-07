111ded9d38cb5d780a438fc5af0636629743fa40
Austin <austin.cotant@gmail.com>
2018-11-06 16:24:18 -0800

    Finish creators page

--- frontend/src/components/creators/creator.jsx
+++ frontend/src/components/creators/creator.jsx
@@ -4,14 +4,21 @@ import { Link } from 'react-router-dom';
 export class Creator extends Component {
   render() {
     const {creator} = this.props
-    return (
-        <li className='creator'>
-            <img scr={creator.imgUrl}></img>
-            <Link to={creator.github}>Github</Link>
-            <Link to={creator.angel}>Angel List</Link>
-            <Link to={creator.linkedin}>Linked In</Link>
-        </li>
-    )
+    return <li>
+        <a href={creator.portfolio}>
+            <h2>{creator.name}</h2>
+            <img src={creator.imgUrl} />
+        </a>
+        <div className="external-links">
+          <a href={creator.github}>
+                <i className="fab fa-github" />
+          </a>
+            <a href={creator.angel}>
+                <i class="fab fa-angellist"></i></a>
+            <a href={creator.linkedin}>
+                <i class="fab fa-linkedin"></i></a>
+        </div>
+      </li>;
   }
 }
 
--- frontend/src/components/creators/creators_page.jsx
+++ frontend/src/components/creators/creators_page.jsx
@@ -10,33 +10,37 @@ export class CreatorsPage extends Component {
         name: 'Austin', 
         github: '',
         angel: '',
+        portfolio: '',
         linkedin: '',
-        imgUrl: ''
+        imgUrl: 'https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/003/013/medium/Austin_Cotant_.jpg?1534882621'
     },
       {
         name: 'Peter', 
         github: '',
         angel: '',
+        portfolio: '',
         linkedin: '',
-        imgUrl: ''
+        imgUrl: 'https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/002/973/medium/Peter_Neimeier_2.jpg?1534890984'
     },
       {
         name: 'Anthony', 
         github: '',
         angel: '',
+        portfolio: '',
         linkedin: '',
-        imgUrl: ''
+        imgUrl: 'https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/002/966/medium/Anthony_Tam.jpg?1534882601'
     },
       {
         name: 'James', 
         github: '',
         angel: '',
+        portfolio: '',
         linkedin: '',
-        imgUrl: ''
+        imgUrl: 'https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/002/972/medium/James_Touri.jpg?1534886869'
     },
     ]
     return (
-      <div className='all-content content'>
+      <div className='all-content creators'>
         <ul>
           {creators.map(creator => (
             <Creator creator={creator} />
